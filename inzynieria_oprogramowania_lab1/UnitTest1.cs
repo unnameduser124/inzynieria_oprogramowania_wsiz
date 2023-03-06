b0042f68b67d7879a4a9c33959f7c93e4b39fd92
ozarskiw <ozarskiw@gmail.com>
2023-03-06 14:15:27 +0100

    Initial commit

---
+++ inzynieria_oprogramowania_lab1/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace inzynieria_oprogramowania_lab1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
